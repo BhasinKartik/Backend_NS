7d1f21dcc01acc3049d9730230cdb5384cdcacb3
Kartik <kbhasin2003@gmail.com>
2024-06-08 18:07:43 +0530

    index.js

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ app.use(cors());
 const port=process.env.PORT;
 // respond with "hello world" when a GET request is made to the homepage
 
-// app.get('/', (req, res) => {
-//   res.send('hello world')
-// })
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
 
 //available routes;
 app.use('/api/auth',require('./routes/auth'));
